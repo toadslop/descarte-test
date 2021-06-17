56f61291d2a598045a4ef168946a5e013269be19
Brian Heise <bnheise@gmail.com>
2021-06-17 10:45:40 +0900

    added additional comments to main.mjs

--- main.mjs
+++ main.mjs
@@ -1,13 +1,15 @@
 import Deck from "./Deck.mjs";
 import { loggingRequested, logIteration } from "./utils.mjs";
 
+// Initialize original deck as new instance of Deck class.
+// See Deck.mjs for more information.
 const originalDeck = new Deck();
-let finalDeck = originalDeck.shuffle();
+if (loggingRequested) logIteration(0, originalDeck);
 
+// Call the shuffle method, which implements the requested algorithm.
+let finalDeck = originalDeck.shuffle();
 // Start the shuffle counter at 1 because finalDeck is initialized as the first shuffle
 let shuffleCount = 1;
-
-if (loggingRequested) logIteration(0, originalDeck);
 
 while (!originalDeck.equals(finalDeck)) {
   if (loggingRequested) logIteration(shuffleCount, finalDeck)
