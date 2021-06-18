6283d5a8f1e53c5fafa873511bcbe9ecf8158bf5
Brian Heise <bnheise@gmail.com>
2021-06-18 10:07:21 +0900

    added extra utils for handling logging and counting

--- utils.mjs
+++ utils.mjs
@@ -1,6 +1,27 @@
-export const loggingRequested = () => process.argv.includes('--with-logging');
-export const logIteration = (count, deck) => {
-    console.log(`Current iteration: ${count}`);
-    console.log(deck.cards);
-    console.log("\n");
+import EventEmitter from "events";
+
+// Logging methods
+export const loggingRequested = () => process.argv.includes("--with-logging");
+export const logShuffle = (cards, count) =>
+  console.log(`Current iteration: ${count}\n`, cards, "\n\n");
+export const logResult = (finalDeck, counter) => {
+  console.info(
+    `Total number of shuffles to return to original state: ${counter.next()
+      .value}`
+  );
+  console.info("Final iteration:");
+  console.info(finalDeck.cards);
+};
+
+// Set up a counter to track number of shuffles
+function* counterGenerator() {
+  let count = 0;
+  while (true) {
+    yield count++;
+  }
 }
+
+export const shuffleCounter = counterGenerator();
+
+// Defined event emitter to track shuffles. It's here to keep the Deck class clean.
+export const event = new EventEmitter();
