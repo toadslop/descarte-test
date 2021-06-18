aec426371db8f9f06a832f0f27883da8352a69cb
Brian Heise <bnheise@gmail.com>
2021-06-18 10:06:13 +0900

    added some methods to generate errors to get the long message out of the Deck class to improve readability

---
+++ errors.mjs
@@ -0,0 +1,19 @@
+// Long error messages clutter up class logic. Therefore I define methods here to generate
+// the error rather than hardcoding it in the class itself.
+
+export const arrayRequired = cards =>
+  new TypeError(
+    `Deck must be initialized with an array but received ${typeof cards}`
+  );
+
+export const fiftyThreeRequired = cards =>
+  new RangeError(
+    `Deck class expects to be initialized with an array length 53 but received an array of length ${cards.length}`
+  );
+
+export const wrongType = deck =>
+  new TypeError(
+    `Input of Deck equals method requires an instance of Deck but received ${Object.getPrototypeOf(
+      deck
+    )}`
+  );
