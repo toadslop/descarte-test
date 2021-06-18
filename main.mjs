6718b16202967de0380a879f66a7e4f42d0a3ab0
Brian Heise <bnheise@gmail.com>
2021-06-18 10:06:44 +0900

    implemented the event listener to handle logging

--- main.mjs
+++ main.mjs
@@ -1,24 +1,25 @@
 import Deck from "./Deck.mjs";
-import { loggingRequested, logIteration } from "./utils.mjs";
+import {
+  loggingRequested,
+  logShuffle,
+  shuffleCounter,
+  logResult,
+  event,
+} from "./utils.mjs";
+import { SHUFFLE } from "constants.mjs";
+
+// Listener to handle logging
+if (loggingRequested()) event.on(SHUFFLE, logShuffle);
 
 // Initialize original deck as new instance of Deck class.
-// See Deck.mjs for more information.
 const originalDeck = new Deck();
-if (loggingRequested()) logIteration(0, originalDeck);
 
-// Call the shuffle method, which implements the requested algorithm.
+// Initialize finalDeck with call the shuffle method, which implements the requested algorithm.
 let finalDeck = originalDeck.shuffle();
-// Start the shuffle counter at 1 because finalDeck is initialized as the first shuffle
-let shuffleCount = 1;
 
+// Keep shuffling until finalDeck equals originalDeck.
 while (!originalDeck.equals(finalDeck)) {
-  if (loggingRequested()) logIteration(shuffleCount, finalDeck)
   finalDeck = finalDeck.shuffle();
-  shuffleCount += 1;
 }
 
-if (loggingRequested()) logIteration(shuffleCount, finalDeck)
-
-console.info(
-  `Total number of shuffles to return to original state: ${shuffleCount}`
-);
+logResult(finalDeck, shuffleCounter);
