72fe489603540738948d1fbb1bc1cec4888b2e51
Brian Heise <bnheise@gmail.com>
2021-06-20 21:16:27 +0900

    moved some helper functions outside of Deck class and into utils; also deleted comments because they are ugly :p

--- Deck.mjs
+++ Deck.mjs
@@ -3,37 +3,30 @@ import { arrayRequired, fiftyThreeRequired, requiresDeck } from "./errors.mjs";
 import { SHUFFLE } from "./constants.mjs";
 
 export default class Deck {
-  // The constructor generates a new deck or accepts an array as a deck
   constructor(cards = Deck.generate()) {
     if (!Array.isArray(cards)) throw arrayRequired(cards);
     if (cards.length !== 53) throw fiftyThreeRequired(cards);
     this._cards = cards;
   }
 
-  // Shuffles the deck and returns a new deck.
   shuffle() {
-    event.emit(SHUFFLE, this.cards, shuffleCounter.next().value); // For logging purposes
+    event.emit(SHUFFLE, this.cards, shuffleCounter.next().value);
     const [lowerHalf, upperHalf] = split(this.cards);
     return new Deck(zip(lowerHalf.reverse(), upperHalf.reverse()));
   }
 
-  // Alias for length for purely semantic purposes
   get count() {
     return this.cards.length;
   }
 
-  // This is used for initializing the deck array when no initial deck array is provided
   static generate = () => [...Array(53).keys()].map(i => i + 1);
 
-  // A helper method that allows copying a deck without a reference
   static copy = deck => new Deck([...deck.cards]);
 
-  // Getter method for the cards array.
   get cards() {
     return this._cards;
   }
 
-  // Equality checker for decks.
   equals(deck) {
     if (!(deck instanceof Deck)) throw requiresDeck(deck);
     return (
--- utils.mjs
+++ utils.mjs
@@ -2,8 +2,10 @@ import EventEmitter from "events";
 
 // Logging methods
 export const loggingRequested = () => process.argv.includes("--with-logging");
+
 export const logShuffle = (cards, count) =>
   console.log(`Current iteration: ${count}\n`, cards, "\n\n");
+
 export const logResult = (finalDeck, counter) => {
   console.info(
     `Total number of shuffles to return to original state: ${counter.next()
@@ -13,7 +15,6 @@ export const logResult = (finalDeck, counter) => {
   console.info(finalDeck.cards);
 };
 
-// Set up a counter to track number of shuffles
 function* counterGenerator() {
   let count = 0;
   while (true) {
@@ -23,7 +24,6 @@ function* counterGenerator() {
 
 export const shuffleCounter = counterGenerator();
 
-// Defined event emitter to track shuffles. It's here to keep the Deck class clean.
 export const event = new EventEmitter();
 
 export const split = array => [
